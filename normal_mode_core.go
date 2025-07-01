f5f10acb02772b7dce636d256de0c8cdceb2d76a
Bartłomiej Klimczak <bklimczak@developer20.com>
2025-07-01 10:05:22 +0000

    Refactor: Use registerCmd for non-repeatable and registerRepeatableCmd for repeatable normal mode commands. Clarifies repeat logic and intent.

--- normal_mode_core.go
+++ normal_mode_core.go
@@ -11,50 +11,74 @@ type normalmode struct {
 	commands    map[string]normalCommand
 	buffer      string
 	lastCommand string
+	repeatableCommands map[string]bool
 }
 
-func NewNormalMode() normalmode {
-	nm := normalmode{}
-	nm.commands = map[string]normalCommand{
-		// basic navigation
-		"j":     nm.commandDown,
-		"down":  nm.commandDown,
-		"k":     nm.commandUp,
-		"up":    nm.commandUp,
-		"h":     nm.commandLeft,
-		"left":  nm.commandLeft,
-		"l":     nm.commandRight,
-		"right": nm.commandRight,
-		"w":     nm.commandNextWord,
-		"b":     nm.commandPrevWord,
-
-		"gg": nm.commandGoToBeginingOfTheFile,
-		"ge": nm.commandGoToEndOfTheFile,
-		"gl": nm.commandGoToLast,
-		"gs": nm.commandGoToFirstNonWhiteCharacter,
-
-		"dd": nm.commandDeleteLine,
-
-		"esc": nm.commandClearBuffer,
-		":":   nm.commandEnterCommandMode,
-		"i":   nm.commandEnterInsertMode,
-		"o":   nm.commandOpenLineBelow,
-		"O":   nm.commandOpenLineAbove,
-
-		"zt": nm.commandTopViewport,
-		"zz": nm.commandCenterViewport,
-		"zb": nm.commandBottomViewport,
-	}
-
+func NewNormalMode() *normalmode {
+	nm := &normalmode{}
+	nm.setupCommands()
 	return nm
 }
 
-func (nm normalmode) Handle(msg tea.KeyMsg, m model) (normalmode, tea.Model, tea.Cmd) {
+func (nm *normalmode) registerCmd(key string, cmd normalCommand) {
+	nm.commands[key] = cmd
+}
+
+func (nm *normalmode) registerRepeatableCmd(key string, cmd normalCommand) {
+	nm.commands[key] = cmd
+	nm.repeatableCommands[key] = true
+}
+
+func (nm *normalmode) setupCommands() {
+	nm.commands = make(map[string]normalCommand)
+	nm.repeatableCommands = make(map[string]bool)
+	
+	// Navigation commands
+	nm.registerCmd("j", nm.commandDown)
+	nm.registerCmd("down", nm.commandDown)
+	nm.registerCmd("k", nm.commandUp)
+	nm.registerCmd("up", nm.commandUp)
+	nm.registerCmd("h", nm.commandLeft)
+	nm.registerCmd("left", nm.commandLeft)
+	nm.registerCmd("l", nm.commandRight)
+	nm.registerCmd("right", nm.commandRight)
+	nm.registerCmd("w", nm.commandNextWord)
+	nm.registerCmd("b", nm.commandPrevWord)
+	
+	// File navigation
+	nm.registerCmd("gg", nm.commandGoToBeginingOfTheFile)
+	nm.registerCmd("ge", nm.commandGoToEndOfTheFile)
+	nm.registerCmd("gl", nm.commandGoToLast)
+	nm.registerCmd("gs", nm.commandGoToFirstNonWhiteCharacter)
+	
+	// Editing commands
+	nm.registerRepeatableCmd("dd", nm.commandDeleteLine)
+	nm.registerRepeatableCmd("o", nm.commandOpenLineBelow)
+	nm.registerRepeatableCmd("O", nm.commandOpenLineAbove)
+	
+	// Mode switching
+	nm.registerCmd("esc", nm.commandClearBuffer)
+	nm.registerCmd(":", nm.commandEnterCommandMode)
+	nm.registerCmd("i", nm.commandEnterInsertMode)
+	
+	// Viewport commands
+	nm.registerCmd("zt", nm.commandTopViewport)
+	nm.registerCmd("zz", nm.commandCenterViewport)
+	nm.registerCmd("zb", nm.commandBottomViewport)
+	
+	// Repeat command
+	nm.registerCmd(".", nm.commandRepeat)
+}
+
+func (nm *normalmode) Handle(msg tea.KeyMsg, m model) (*normalmode, tea.Model, tea.Cmd) {
 	buff := nm.buffer + msg.String()
 	nCommand, ok := nm.commands[buff]
 	if ok {
 		m, cmd := nCommand(m, nil)
-		nm.lastCommand = buff
+		// Only store repeatable commands
+		if nm.repeatableCommands[buff] {
+			nm.lastCommand = buff
+		}
 		nm.buffer = ""
 		return nm, m, cmd
 	}
@@ -75,4 +99,18 @@ func (nm normalmode) Handle(msg tea.KeyMsg, m model) (normalmode, tea.Model, tea
 	}
 
 	return nm, m, nil
+}
+
+func (nm *normalmode) commandRepeat(m model, cmd tea.Cmd) (tea.Model, tea.Cmd) {
+	// If there's no last command, do nothing
+	if nm.lastCommand == "" {
+		return m, cmd
+	}
+
+	// Find and execute the last command
+	if nCommand, ok := nm.commands[nm.lastCommand]; ok {
+		return nCommand(m, cmd)
+	}
+
+	return m, cmd
 } 
--- normal_mode_test.go
+++ normal_mode_test.go
@@ -169,7 +169,177 @@ func TestCommandClearBuffer(t *testing.T) {
 	newM := newModel.(model)
 	
 	// Check that buffer was cleared
-	if newM.normalmode.(normalmode).buffer != "" {
-		t.Errorf("Expected buffer to be cleared, got '%s'", newM.normalmode.(normalmode).buffer)
+	if newM.normalmode.buffer != "" {
+		t.Errorf("Expected buffer to be cleared, got '%s'", newM.normalmode.buffer)
+	}
+}
+
+func TestCommandRepeat(t *testing.T) {
+	nm := NewNormalMode()
+	
+	// Create a buffer with some content
+	style := newEditorStyle()
+	buff := newBuffer(style)
+	buff = buff.ReplaceLine(0, "line 1")
+	buff = buff.AppendLine("line 2")
+	buff = buff.AppendLine("line 3")
+	buff = buff.SetCursorX(3)
+	buff = buff.SetCursorY(1)
+	
+	m := model{
+		buffers:  []buffer{buff},
+		currBuffer: 0,
+		mode:     ModeNormal,
+		normalmode: nm,
+	}
+	
+	// First, press 'd' (should buffer)
+	newNM, newModel, _ := m.normalmode.Handle(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'d'}}, m)
+	m = newModel.(model)
+	m.normalmode = newNM
+
+	// Then, press 'd' again (should execute dd)
+	newNM, newModel, _ = m.normalmode.Handle(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'d'}}, m)
+	m = newModel.(model)
+	m.normalmode = newNM
+
+	// Check that a line was deleted
+	if m.buffers[0].NoOfLines() != 2 {
+		t.Errorf("Expected 2 lines after dd command, got %d", m.buffers[0].NoOfLines())
+	}
+	// Check that lastCommand was set
+	if m.normalmode.lastCommand != "dd" {
+		t.Errorf("Expected lastCommand to be 'dd', got '%s'", m.normalmode.lastCommand)
+	}
+	// Now test the repeat command (.)
+	newNM, newModel, _ = m.normalmode.Handle(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'.'}}, m)
+	m = newModel.(model)
+	m.normalmode = newNM
+	// Check that another line was deleted
+	if m.buffers[0].NoOfLines() != 1 {
+		t.Errorf("Expected 1 line after repeat command, got %d", m.buffers[0].NoOfLines())
+	}
+}
+
+func TestCommandRepeatNoLastCommand(t *testing.T) {
+	nm := NewNormalMode()
+	
+	// Create a buffer with some content
+	style := newEditorStyle()
+	buff := newBuffer(style)
+	buff = buff.ReplaceLine(0, "line 1")
+	buff = buff.SetCursorX(3)
+	buff = buff.SetCursorY(0)
+	
+	m := model{
+		buffers:  []buffer{buff},
+		currBuffer: 0,
+		mode:     ModeNormal,
+		normalmode: nm,
+	}
+	
+	// Test repeat command with no last command
+	newNM, newModel, _ := m.normalmode.Handle(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'.'}}, m)
+	newM := newModel.(model)
+	newM.normalmode = newNM
+	// Check that nothing changed (no last command to repeat)
+	if newM.buffers[0].NoOfLines() != 1 {
+		t.Errorf("Expected 1 line after repeat command with no last command, got %d", newM.buffers[0].NoOfLines())
+	}
+	if newM.buffers[0].Line(0) != "line 1" {
+		t.Errorf("Expected line to remain unchanged, got '%s'", newM.buffers[0].Line(0))
+	}
+}
+
+func TestNavigationCommandsNotRepeatable(t *testing.T) {
+	nm := NewNormalMode()
+	
+	// Create a buffer with some content
+	style := newEditorStyle()
+	buff := newBuffer(style)
+	buff = buff.ReplaceLine(0, "line 1")
+	buff = buff.AppendLine("line 2")
+	buff = buff.SetCursorX(3)
+	buff = buff.SetCursorY(0)
+	
+	m := model{
+		buffers:  []buffer{buff},
+		currBuffer: 0,
+		mode:     ModeNormal,
+		normalmode: nm,
+	}
+	
+	// Test that navigation command 'j' is not repeatable
+	newNM, newModel, _ := m.normalmode.Handle(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'j'}}, m)
+	m = newModel.(model)
+	m.normalmode = newNM
+	
+	// Check that cursor moved down
+	if m.buffers[0].cursorY != 1 {
+		t.Errorf("Expected cursor Y to be 1 after 'j' command, got %d", m.buffers[0].cursorY)
+	}
+	
+	// Check that lastCommand was NOT set (navigation commands are not repeatable)
+	if m.normalmode.lastCommand != "" {
+		t.Errorf("Expected lastCommand to be empty after navigation command, got '%s'", m.normalmode.lastCommand)
+	}
+	
+	// Test repeat command - should do nothing since no repeatable command was executed
+	newNM, newModel, _ = m.normalmode.Handle(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'.'}}, m)
+	m = newModel.(model)
+	m.normalmode = newNM
+	
+	// Check that cursor position didn't change (no repeat occurred)
+	if m.buffers[0].cursorY != 1 {
+		t.Errorf("Expected cursor Y to remain 1 after repeat with no repeatable command, got %d", m.buffers[0].cursorY)
+	}
+}
+
+func TestEditingCommandsAreRepeatable(t *testing.T) {
+	nm := NewNormalMode()
+	
+	// Create a buffer with some content
+	style := newEditorStyle()
+	buff := newBuffer(style)
+	buff = buff.ReplaceLine(0, "line 1")
+	buff = buff.AppendLine("line 2")
+	buff = buff.AppendLine("line 3")
+	buff = buff.SetCursorX(3)
+	buff = buff.SetCursorY(1)
+	
+	m := model{
+		buffers:  []buffer{buff},
+		currBuffer: 0,
+		mode:     ModeNormal,
+		normalmode: nm,
+	}
+	
+	// Test that editing command 'dd' is repeatable
+	newNM, newModel, _ := m.normalmode.Handle(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'d'}}, m)
+	m = newModel.(model)
+	m.normalmode = newNM
+
+	newNM, newModel, _ = m.normalmode.Handle(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'d'}}, m)
+	m = newModel.(model)
+	m.normalmode = newNM
+	
+	// Check that a line was deleted
+	if m.buffers[0].NoOfLines() != 2 {
+		t.Errorf("Expected 2 lines after dd command, got %d", m.buffers[0].NoOfLines())
+	}
+	
+	// Check that lastCommand was set (editing commands are repeatable)
+	if m.normalmode.lastCommand != "dd" {
+		t.Errorf("Expected lastCommand to be 'dd', got '%s'", m.normalmode.lastCommand)
+	}
+	
+	// Test repeat command - should delete another line
+	newNM, newModel, _ = m.normalmode.Handle(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'.'}}, m)
+	m = newModel.(model)
+	m.normalmode = newNM
+	
+	// Check that another line was deleted
+	if m.buffers[0].NoOfLines() != 1 {
+		t.Errorf("Expected 1 line after repeat command, got %d", m.buffers[0].NoOfLines())
 	}
 } 
